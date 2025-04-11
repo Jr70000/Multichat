4265c4ef7da661bdbdd8bc460aa05604c00c2589
Victor Nicholas <vcnicholas982@gmail.com>
2025-04-11 16:31:12 +0100

    loocator

--- lib/data/core/network_config.dart
+++ lib/data/core/network_config.dart
@@ -1,6 +1,6 @@
 //this class has all remote service configurations, and it takes care of the different environments we have
 
 class NetworkConfig {
-  static String DEVELOP_BASE_URL = "https://bucx-api-1.onrender.com";
-  static String RELEASE_BASE_URL = "https://bucx-api-1.onrender.com";
+  static String DEVELOP_BASE_URL = "https://";
+  static String RELEASE_BASE_URL = "https://";
 }
